565712fdd02b366784c77d8b5a8657a4538400c0
wachirawit autamakorn <wachirawitautamakorn@gmail.com>
2024-06-19 13:54:39 +0700

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private page1: NavController) {}
+
+gotologin(){
+ this.page1.navigateForward('/login');
+} 
+}
